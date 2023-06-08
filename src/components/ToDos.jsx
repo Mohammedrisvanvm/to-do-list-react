d53857704f1366fa04d27ce06eef1a12a1925bf7
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-08 10:09:25 +0530

    todos created

---
+++ src/components/ToDos.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function ToDos({ title, toDo, value, setToDo }) {
+  const deleteHandler = () => {
+    setToDo(toDo.filter((elem) => elem.id !== value.id));
+  };
+
+  const toggler = () => {
+    setToDo(
+      toDo.map((elem) => {
+        if (elem.id === value.id) {
+          return {
+            ...elem,
+            record: !elem.record,
+          };
+        }
+        return elem;
+      })
+    );
+  };
+
+  return (
+    <div className="todo">
+      <li className={`todo-item ${value.record ? "completed" :""}`}>{title}</li>
+      <button onClick={toggler} className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button onClick={deleteHandler} className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+}
+
+export default ToDos;
